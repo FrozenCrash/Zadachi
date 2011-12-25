9c3e8e626c7e66849058a8080f1ae40075656fd9
FrozenCrash <zzcrash@gmail.com>
2011-12-25 15:04:28 +0200

    math::PI

--- 2.rb
+++ 2.rb
@@ -1,17 +1,17 @@
 r1=10
 r2=20
 h=30
-l = Math.sqrt(h**2+(r1-r2)**2)
+l = Math.sqrt(h**2 + (r1-r2)**2)
 def l(h, r1, r2)
-  Math.sqrt(h**2+(r1-r2)**2)
+  Math.sqrt(h**2 + (r1-r2)**2)
 end
-v = Math::PI*h/ 3*(r2**2+ r1**2+r2*r1)
+  v = Math::PI*h/3*(r2**2 + r1**2 + r2*r1)
 def v(h, r1, r2)
-Math:pi*h/3*(r2**2+r1**2+r2*r1)
+  Math:pi*h/3*(r2**2+r1**2+r2*r1) #Try to write (Math::PI => pi) but nothing. ERROR
 end
-s = Math::PI*3*(r2+r2)+Math::PI*(r2**2+r1**2) #math PI
+  s = Math::PI*3*(r2+r2) + Math::PI*(r2**2+r1**2) #math PI
 def s(l, r1, r2)
-math::pi*3*(r2+r1)+math::pi*(r2**2+r1**2)
+  Math::PI*3*(r2+r1) + Math::PI*(r2**2+r1**2) 
 end
 
 puts v
